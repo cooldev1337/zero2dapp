7f01c30b62dcdce6fb5408a2529206b5aa4778ab
cooldev1337 <cooldev1337@proton.me>
2025-11-20 15:54:21 -0300

    Token name changed

--- contracts/BAToken.sol
+++ contracts/BAToken.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.27;
 import {ERC20} from "@openzeppelin/contracts@5.4.0/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.4.0/access/AccessControl.sol";
 
-contract BuenoToken is ERC20, AccessControl {
+contract BAToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor()
-        ERC20("BuenoToken", "BTK")
+        ERC20("BA Token", "BAt")
     {
     	_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     	_grantRole(MINTER_ROLE, msg.sender);        
